9daa2a41242003342c14f38c61ebe721f5c28df2
Alexandr-Borodin <70877579+Alexandr-Borodin@users.noreply.github.com>
2022-04-10 15:37:30 +0300

    Template has been added.

---
+++ PGZ-Course-Work-master/DisposeGame/Scripts/Environment/EnvironmentScript.cs
@@ -0,0 +1,25 @@
+﻿using GameEngine.Collisions;
+using GameEngine.Graphics;
+using GameEngine.Scripts;
+using SharpDX;
+using System;
+
+namespace DisposeGame.Scripts.Environment
+{
+    public class EnvironmentScript : Script
+    {
+        private Vector3 _direction;
+        private float _speed;
+
+        public EnvironmentScript(Vector3 direction, float speed)
+        {
+            _direction = direction;
+            _speed = speed;
+        }
+
+        public override void Update(float delta)
+        {
+            GameObject.MoveBy(_direction * _speed);
+        }
+    }
+}
